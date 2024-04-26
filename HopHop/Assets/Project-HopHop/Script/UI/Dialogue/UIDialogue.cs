860229e8420353c9add232fbb607d7e82945eb17
PDK13 <khoa199960@gmail.com>
2024-04-26 13:54:35 +0700

    Update UIDialogue.cs

--- HopHop/Assets/Project-HopHop/Script/UI/Dialogue/UIDialogue.cs
+++ HopHop/Assets/Project-HopHop/Script/UI/Dialogue/UIDialogue.cs
@@ -10,8 +10,16 @@ public class UIDialogue : MonoBehaviour
     [SerializeField] private Transform m_dialogueAuthor;
     [SerializeField] private Transform m_dialogueNone;
 
+    private Image m_imgDialogueAuthor;
+    private TextMeshProUGUI m_tmpDialogueAuthor;
+    private TextMeshProUGUI m_tmpDialogueNone;
+
     private void Awake()
     {
+        m_imgDialogueAuthor = m_dialogueAuthor.GetComponentInChildren<Image>();
+        m_tmpDialogueAuthor = m_dialogueAuthor.GetComponentInChildren<TextMeshProUGUI>();
+        m_tmpDialogueNone = m_dialogueNone.GetComponentInChildren<TextMeshProUGUI>();
+
         DialogueManager.Instance.onStage += OnDialogueStage;
         DialogueManager.Instance.onText += OnDialogueText;
     }
@@ -52,15 +60,15 @@ public class UIDialogue : MonoBehaviour
             m_dialogueAuthor.gameObject.SetActive(true);
             m_dialogueNone.gameObject.SetActive(false);
 
-            m_dialogueAuthor.GetComponentInChildren<Image>().sprite = Avatar;
-            DialogueManager.Instance.Tmp = m_dialogueAuthor.GetComponentInChildren<TextMeshProUGUI>();
+            m_imgDialogueAuthor.sprite = Avatar;
+            DialogueManager.Instance.Tmp = m_tmpDialogueAuthor;
         }
         else
         {
             m_dialogueAuthor.gameObject.SetActive(false);
             m_dialogueNone.gameObject.SetActive(true);
 
-            DialogueManager.Instance.Tmp = m_dialogueNone.GetComponentInChildren<TextMeshProUGUI>();
+            DialogueManager.Instance.Tmp = m_tmpDialogueNone;
         }
     }
 }
