ef84589c4d29835bf8e58c167ae0b7505d6a6bcd
PDK13 <khoa199960@gmail.com>
2024-04-26 10:11:11 +0700

    update event config

--- HopHop/Assets/Project-HopHop/Script/Config/EventConfigSingle.cs
+++ HopHop/Assets/Project-HopHop/Script/Config/EventConfigSingle.cs
@@ -6,26 +6,62 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "event-config-single", menuName = "HopHop/Event Config Single", order = 0)]
 public class EventConfigSingle : ScriptableObject
 {
-    public List<EventConfigSingleData> Data = new List<EventConfigSingleData>();
+    public List<EventConfigSingleData> Data;
 }
 
 [Serializable]
-public class EventConfigSingleData
+public struct EventConfigSingleData
 {
+    [SerializeField] private bool m_wait;
+
     public DialogueConfigSingle Dialogue;
-    public List<string> Command;
+    public List<EventConfigSingleDataCommand> Command;
+    public List<EventConfigSingleDataChoice> Choice;
+
+    public bool Wait
+    {
+        get
+        {
+            if (m_wait)
+                return true;
+
+            if (Dialogue != null)
+                return true;
+
+            if (Choice != null ? Choice.Count > 0 : false)
+                return true;
+
+            return false;
+        }
+    }
 }
 
 [Serializable]
-public class EventConfigSingleDataChoice
+public struct EventConfigSingleDataCommand
+{
+    public EventCommandType Type;
+    public string Invoke;
+}
+
+[Serializable]
+public struct EventConfigSingleDataChoice
 {
     public string Name;
     public DialogueConfigSingle Dialogue;
+
+    public EventConfigSingle EventNext;
+}
+
+public enum EventCommandType
+{
+    None = 0,
+    Spawm = 1,
+    Move = 2,
 }
 
 #if UNITY_EDITOR
 
-[CustomEditor(typeof(EventConfigSingle))]
+//[CustomEditor(typeof(EventConfigSingle))]
 public class EventConfigEditor : Editor
 {
     private const float POPUP_HEIGHT = 150f * 2;
@@ -43,14 +79,14 @@ public class EventConfigEditor : Editor
     {
         m_target = target as EventConfigSingle;
         //
-        m_dataCount = m_target.Data.Count;
-        m_dataCommandCount = new List<int>();
-        m_dataCommandShow = new List<bool>();
-        foreach (var Item in m_target.Data)
-        {
-            m_dataCommandCount.Add(Item.Command.Count);
-            m_dataCommandShow.Add(false);
-        }
+        //m_dataCount = m_target.Data.Count;
+        //m_dataCommandCount = new List<int>();
+        //m_dataCommandShow = new List<bool>();
+        //foreach (var Item in m_target.Data)
+        //{
+        //    m_dataCommandCount.Add(Item.Command.Count);
+        //    m_dataCommandShow.Add(false);
+        //}
     }
 
     public override void OnInspectorGUI()
@@ -62,74 +98,74 @@ public class EventConfigEditor : Editor
 
     private void SetGUIGroupData()
     {
-        QUnityEditor.SetLabel("EVENT", QUnityEditor.GetGUIStyleLabel(FontStyle.Bold));
+        //QUnityEditor.SetLabel("EVENT", QUnityEditor.GetGUIStyleLabel(FontStyle.Bold));
 
-        #region COUNT
-        m_dataCount = QUnityEditor.SetGroupNumberChangeLimitMin(m_dataCount, 0);
-        while (m_dataCount > m_target.Data.Count)
-        {
-            m_target.Data.Add(new EventConfigSingleData());
-            m_dataCommandCount.Add(0);
-            m_dataCommandShow.Add(false);
-        }
-        while (m_dataCount < m_target.Data.Count)
-        {
-            m_target.Data.RemoveAt(m_target.Data.Count - 1);
-            m_dataCommandCount.RemoveAt(m_dataCommandCount.Count - 1);
-            m_dataCommandShow.RemoveAt(m_dataCommandShow.Count - 1);
-        }
-        #endregion
+        //#region COUNT
+        //m_dataCount = QUnityEditor.SetGroupNumberChangeLimitMin(m_dataCount, 0);
+        //while (m_dataCount > m_target.Data.Count)
+        //{
+        //    m_target.Data.Add(new EventConfigSingleData());
+        //    m_dataCommandCount.Add(0);
+        //    m_dataCommandShow.Add(false);
+        //}
+        //while (m_dataCount < m_target.Data.Count)
+        //{
+        //    m_target.Data.RemoveAt(m_target.Data.Count - 1);
+        //    m_dataCommandCount.RemoveAt(m_dataCommandCount.Count - 1);
+        //    m_dataCommandShow.RemoveAt(m_dataCommandShow.Count - 1);
+        //}
+        //#endregion
 
-        m_scrollData = QUnityEditor.SetScrollViewBegin(m_scrollData, null);
-        for (int i = 0; i < m_target.Data.Count; i++)
-        {
-            #region ITEM
-            QUnityEditor.SetHorizontalBegin();
-            QUnityEditor.SetLabel(i.ToString(), QUnityEditor.GetGUIStyleLabel(FontStyle.Normal), QUnityEditor.GetGUILayoutWidth(25));
+        //m_scrollData = QUnityEditor.SetScrollViewBegin(m_scrollData, null);
+        //for (int i = 0; i < m_target.Data.Count; i++)
+        //{
+        //    #region ITEM
+        //    QUnityEditor.SetHorizontalBegin();
+        //    QUnityEditor.SetLabel(i.ToString(), QUnityEditor.GetGUIStyleLabel(FontStyle.Normal), QUnityEditor.GetGUILayoutWidth(25));
 
-            #region ITEM - MAIN
-            QUnityEditor.SetVerticalBegin();
+        //    #region ITEM - MAIN
+        //    QUnityEditor.SetVerticalBegin();
 
-            #region ITEM - MAIN - DIALOGUE
-            QUnityEditor.SetHorizontalBegin();
-            QUnityEditor.SetLabel("Dialogue", null, QUnityEditor.GetGUILayoutWidth(LABEL_WIDTH));
-            m_target.Data[i].Dialogue = QUnityEditor.SetField(m_target.Data[i].Dialogue);
-            QUnityEditor.SetHorizontalEnd();
-            #endregion
+        //    #region ITEM - MAIN - DIALOGUE
+        //    QUnityEditor.SetHorizontalBegin();
+        //    QUnityEditor.SetLabel("Dialogue", null, QUnityEditor.GetGUILayoutWidth(LABEL_WIDTH));
+        //    m_target.Data[i].Dialogue = QUnityEditor.SetField(m_target.Data[i].Dialogue);
+        //    QUnityEditor.SetHorizontalEnd();
+        //    #endregion
 
-            #region ITEM - MAIN - COMMAND
-            if (QUnityEditor.SetButton("Command", QUnityEditor.GetGUIStyleButton(FontStyle.Normal, TextAnchor.MiddleCenter, 10), QUnityEditor.GetGUILayoutWidth(LABEL_WIDTH)))
-                m_dataCommandShow[i] = !m_dataCommandShow[i];
-            if (m_dataCommandShow[i])
-            {
-                m_dataCommandCount[i] = QUnityEditor.SetGroupNumberChangeLimitMin(m_dataCommandCount[i], 0);
-                //
-                while (m_dataCommandCount[i] > m_target.Data[i].Command.Count)
-                    m_target.Data[i].Command.Add("");
-                while (m_dataCommandCount[i] < m_target.Data[i].Command.Count)
-                    m_target.Data[i].Command.RemoveAt(m_target.Data.Count - 1);
-                //
-                QUnityEditor.SetVerticalBegin();
-                for (int j = 0; j < m_target.Data[i].Command.Count; j++)
-                {
-                    QUnityEditor.SetHorizontalBegin();
-                    QUnityEditor.SetLabel(j.ToString(), QUnityEditor.GetGUIStyleLabel(FontStyle.Normal), QUnityEditor.GetGUILayoutWidth(25));
-                    m_target.Data[i].Command[j] = QUnityEditor.SetField(m_target.Data[i].Command[j]);
-                    QUnityEditor.SetHorizontalEnd();
-                }
-                QUnityEditor.SetVerticalEnd();
-            }
-            #endregion
+        //    #region ITEM - MAIN - COMMAND
+        //    if (QUnityEditor.SetButton("Command", QUnityEditor.GetGUIStyleButton(FontStyle.Normal, TextAnchor.MiddleCenter, 10), QUnityEditor.GetGUILayoutWidth(LABEL_WIDTH)))
+        //        m_dataCommandShow[i] = !m_dataCommandShow[i];
+        //    if (m_dataCommandShow[i])
+        //    {
+        //        m_dataCommandCount[i] = QUnityEditor.SetGroupNumberChangeLimitMin(m_dataCommandCount[i], 0);
+        //        //
+        //        while (m_dataCommandCount[i] > m_target.Data[i].Command.Count)
+        //            m_target.Data[i].Command.Add("");
+        //        while (m_dataCommandCount[i] < m_target.Data[i].Command.Count)
+        //            m_target.Data[i].Command.RemoveAt(m_target.Data.Count - 1);
+        //        //
+        //        QUnityEditor.SetVerticalBegin();
+        //        for (int j = 0; j < m_target.Data[i].Command.Count; j++)
+        //        {
+        //            QUnityEditor.SetHorizontalBegin();
+        //            QUnityEditor.SetLabel(j.ToString(), QUnityEditor.GetGUIStyleLabel(FontStyle.Normal), QUnityEditor.GetGUILayoutWidth(25));
+        //            m_target.Data[i].Command[j] = QUnityEditor.SetField(m_target.Data[i].Command[j]);
+        //            QUnityEditor.SetHorizontalEnd();
+        //        }
+        //        QUnityEditor.SetVerticalEnd();
+        //    }
+        //    #endregion
 
-            QUnityEditor.SetHorizontalEnd();
-            #endregion
+        //    QUnityEditor.SetHorizontalEnd();
+        //    #endregion
 
-            QUnityEditor.SetVerticalEnd();
-            #endregion
+        //    QUnityEditor.SetVerticalEnd();
+        //    #endregion
 
-            QUnityEditor.SetSpace();
-        }
-        QUnityEditor.SetScrollViewEnd();
+        //    QUnityEditor.SetSpace();
+        //}
+        //QUnityEditor.SetScrollViewEnd();
     }
 }
 
